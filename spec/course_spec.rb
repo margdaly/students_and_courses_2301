0ed2be0a4d83f3a6c8c78313f2a2cff0cb3d7ef0
margdaly <margaret.daly@gmail.com>
2023-02-15 10:04:12 -0700

    test: Add describe capacity tests

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -9,7 +9,23 @@ RSpec.describe Course do
       expect(course.name).to eq("Calculus")
       expect(course.capacity).to eq(2)
     end
+  end
 
-    # it 'has students'
+  describe '#capacity' do
+    it 'can enroll students' do
+      course = Course.new("Calculus", 2) 
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29}) 
+
+      expect(course.students).to eq([])
+      expect(course.full?).to eq(false)
+
+      course.enroll(student1)
+      course.enroll(student2)
+
+      expect(course.students).to eq([student1, student2])
+      expect(course.full?).to eq(true)
+      expect(course.students.size).to eq(course.capacity)
+    end
   end
 end
