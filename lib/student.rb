83091365f7709bea9c0e37badf92188cf510be36
margdaly <margaret.daly@gmail.com>
2023-02-15 09:29:09 -0700

    feat: Add log_score method to student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,15 @@
 
 class Student
   attr_reader :name, :age
+  attr_accessor :scores
 
   def initialize(student_details)
     @name = student_details[:name]
     @age = student_details[:age]
     @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
   end
 end
