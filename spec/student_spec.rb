7170694330156aa4a3cb2c5b4101b6ef6543715f
margdaly <margaret.daly@gmail.com>
2023-02-15 09:23:16 -0700

    test: Add scores/grade tests

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -10,8 +10,24 @@ RSpec.describe Student do
       expect(student.age).to eq(21)
     end
 
+  describe 'scores/grades' do
     it 'has scores' do
-      
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.scores).to be_an_instance_of(Array)
+      expect(student.log_score(89)).to eq([89])
+      expect(student.log_score(78)).to eq([89, 78])
+      expect(student.scores.include?(34)).to eq(false)
+    end
+
+    xit 'has grades' do
+      student = Student.new({name: "Morgan", age: 21})
+
+      student.log_score(89)
+      student.log_score(78)
+
+      expect(student.grade).to eq(83.5)
+      expect(student.grade).to be_an_instance_of(Float)
     end
   end
 end
