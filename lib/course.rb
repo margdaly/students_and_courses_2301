79ccd1154e4846004ee043eb190e8a5fd86bf472
margdaly <margaret.daly@gmail.com>
2023-02-15 09:54:13 -0700

    Add init to course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,10 @@
+class Course
+  attr_reader :name, :capacity
+  attr_accessor :students
 
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+end
